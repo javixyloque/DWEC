a862ff87524fcac38454f808297a5d340179dda5
javixyloque <perosa1000@gmail.com>
2024-10-14 11:59:49 +0200

    Commit clase 14/10

--- Tema 3/mat.js
+++ Tema 3/mat.js
@@ -1,17 +1,57 @@
 
 
-let arr = [[1,2,3], [4, 5, 6], [7, 8, 9]];
-
-let nuevoarray = [];
-
-for (let i = 0; i < arr.length; i++) {
-
-    for (let j = 0; j < arr[i].length; i++) {
-        if (i == j) {
-            nuevoarray.push(arr[i][j]);
-        }
+const matrizIdentidad = () => {
+    let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];    
+    let nuevoarray = [];
+    for (let i = 0; i < arr[0].length; i++) {
+        nuevoarray[i] = [];
     }
     
+    
+    // RECORREMOS CADA ARRAY DE CADA POSICION DE ARRAY
+    // Y AL NUEVO LE ASIGNAMOS LA POSICION CONTRARIA
+    
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) { 
+            nuevoarray[j][i] = arr[i][j];
+        }
+    }
+    return nuevoarray;
 }
-console.log(nuevoarray);
 
+// CREACION DEL NUEVO ARRAY CON SUBARRAYS EN POSICION I
+
+console.log(matrizIdentidad());
+
+
+const eliminarElemento = () => {
+    let arr = ["jose", "Ander","Caspio", "Lorena"];
+    arr.filter((element) => {
+        if( element =="jose") {
+            return "";
+        } 
+        return element;
+    })
+
+    return arr;
+}
+console.log(eliminarElemento());
+
+
+// OBTENER CUENTA DE LOS ELEMENTOS QUE HAY
+let nums = [2,3,2,2] 
+
+for  (let i = 0; i < nums.length; i++) {
+
+}
+
+const repetidos = (nums) => {
+    let arrNums = nums.reduce(1,2)
+    let digito1 = nums.filter ((element) => {
+        return element == nums[0];
+    })
+
+    
+    return arrNums
+}
+console.log(repetidos(nums));
