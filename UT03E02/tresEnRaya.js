eae9fd3a7985ee64d9a2012ce3e3fbd401fdf16b
javixyloque <perosa1000@gmail.com>
2024-10-15 14:14:50 +0200

    commit clase 15/10

---
+++ UT03E02/tresEnRaya.js
@@ -0,0 +1,21 @@
+let tablero = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+]
+let movimiento = prompt("Escribe el numero de casilla que quieres marcar");
+let turno = 0;
+
+hacerMov(movimiento);
+
+const hacerMov = (mov) => {
+    if (turno % 2===0) {
+
+    }
+    switch (mov) {
+        case mov<3:
+            tablero.push();
+            break;
+    }
+    turno++;
+}
