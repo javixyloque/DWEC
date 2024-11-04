78bbedcf5cba180188b72a6b72dc2e443d115a15
javixyloque <perosa1000@gmail.com>
2024-11-04 11:52:15 +0100

    Retoques fork Javier Alvarez Centeno

--- DOM/3enraya.js
+++ DOM/3enraya.js
@@ -6,27 +6,14 @@ let tablero = [
     ['', '', ''],
     ['', '', '']
 ];
-const combinacionesGanadoras = [
-    // Pongo estos comentarios pq si no me vuelvo loco 
-         // HORIZONTALES
-        [tablero[0][0], tablero[0][1], tablero[0][2]],
-        [tablero[1][0], tablero[1][1], tablero[1][2]],
-        [tablero[2][0], tablero[2][1], tablero[2][2]],
-        //DIAGONALES
-        [tablero[0][0], tablero[1][1], tablero[2][2]],
-        [tablero[2][0], tablero[1][1], tablero[0][2]],
-        //VERTICALES
-        [tablero[0][0], tablero[1][0], tablero[2][0]],
-        [tablero[0][1], tablero[1][1], tablero[2][1]],
-        [tablero[0][2], tablero[1][2], tablero[2][2]]
-]
+
 const CASILLAS_VALIDAS = ["1","2","3","4","5","6","7","8","9"]
 const FICHAS = ["X","O"];
 
-const FILA_IZQUIERDA = [1,4,7]
-const FILA_DERECHA = [3, 6, 9];
-const DIAGONAL_PRINCIPAL = [1, 5, 9];
-const DIAGONAL_SECUNDARIA = [3, 5, 7];
+// const FILA_IZQUIERDA = [1,4,7]
+// const FILA_DERECHA = [3, 6, 9];
+// const DIAGONAL_PRINCIPAL = [1, 5, 9];
+// const DIAGONAL_SECUNDARIA = [3, 5, 7];
 let victoria = false;
 let tablas = false;
 
@@ -48,9 +35,7 @@ function casillaOnClick (e) {
     if (comprobarCasillaValida(casilla)) {
         ejecutarTurno(casilla);
         comprobarFinJuego(casilla);
-    } else {
-
-    }
+    } 
 
 }
 
@@ -74,14 +59,32 @@ function ejecutarTurno (casilla) {
 }
 function comprobarTablero(tablero) {
     // let contador = 0;
+    const combinacionesGanadoras = [
+        // Pongo estos comentarios pq si no me vuelvo loco 
+             // HORIZONTALES
+            [tablero[0][0], tablero[0][1], tablero[0][2]],
+            [tablero[1][0], tablero[1][1], tablero[1][2]],
+            [tablero[2][0], tablero[2][1], tablero[2][2]],
+            //DIAGONALES
+            [tablero[0][0], tablero[1][1], tablero[2][2]],
+            [tablero[2][0], tablero[1][1], tablero[0][2]],
+            //VERTICALES
+            [tablero[0][0], tablero[1][0], tablero[2][0]],
+            [tablero[0][1], tablero[1][1], tablero[2][1]],
+            [tablero[0][2], tablero[1][2], tablero[2][2]]
+    ]
     
-    // NECESITARIA HACER UN TABLERO APARTE PARA HACER ESTO :')
+
     for (let ganar of combinacionesGanadoras) {
         if (ganar[0]===ganar[1] && ganar[0] === ganar[2] && ganar[0] !== ''){
-            return true;
+            victoria =  true;
+            return victoria;
+        } else {
+            victoria = false;
         }
         
     }
+    return victoria;
         
 
 
@@ -93,20 +96,21 @@ function comprobarTablero(tablero) {
 
 
 function comprobarTablas () {
-    if (turnoActual == 9 && !victoria) {
+    if (turnoActual == 9 && !comprobarTablero(tablero)) {
         alert("Empate")
     }
 }
+
 function comprobarFinJuego (casilla) {
     const numeroCasilla = casilla.id.split("-")[1];
-    comprobarTablero(numeroCasilla, casilla, tablero)
+    console.log(comprobarTablero(tablero));
     
 
     comprobarTablas();
     console.log(tablero)
     if  (comprobarTablero(tablero)) {
         // VICTORIA
-        alert (`Gana el jugador ${FICHAS[ turnoActual  %  2  ]}`)
+        alert (`Gana el jugador ${FICHAS[ (turnoActual  %  2) -1  ]}`)
     }
     
 
