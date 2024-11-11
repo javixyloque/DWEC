a8d1a28333fc2f83cc40e80f243754b22e11040c
javixyloque <perosa1000@gmail.com>
2024-11-11 12:10:59 +0100

    sdfgkndklgh

---
+++ array-methods.js
@@ -0,0 +1,18 @@
+let empleados = [
+    {nombre: "Pepe", apellido:"Pepez", sueldo: 5000},
+    {nombre: "Paca", apellido:"Garla", sueldo: 3000},
+    {nombre: "Habib", apellido:"Amil", sueldo: 12098}
+]
+
+let sueldoAlto = empleados.reduce((acum, empleado)=> {
+    return empleado.sueldo > acum ? empleado: acum;
+}, 0)
+
+let arrayOrd = empleados.sort((a, b) => {
+    return b.sueldo - a.sueldo;
+});
+
+
+
+console.log(sueldoAlto);
+console.log(arrayOrd);
