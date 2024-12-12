0aee785aec6e309cded34c40ee715ee521b37733
javixyloque <perosa1000@gmail.com>
2024-12-13 00:39:53 +0100

    LOCALSTORAGE

---
+++ COOKIES_LOCALSTORAGE/prueba.js
@@ -0,0 +1,22 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+   
+
+    localStorage.setItem("persona", "Angelillo");
+    let persona = localStorage.getItem("persona");
+    console.log(persona);
+
+    sessionStorage.setItem("Sesión", "Juanolo");
+    
+    let sesion = sessionStorage.getItem("Sesión"); // METEMOS A "SESIÓN": "JUANOLO" EN UN OBJETO 
+    console.log(sesion); // DEVUELVE JUANOLO
+    let body = document.querySelector('body');
+    let div = document.createElement('div');
+    body.appendChild(div);
+    body.setAttribute('style', 'display: flex; justify-content: center; align-items: center; height: 100vh; background-color: #190933; transition: background-color 1s ease-in-out');
+    div.setAttribute('style', 'color: #F5F0F6; font-size: 24px; font-family: sans-serif; text-align: center; justify-self: center');
+    div.textContent = `Saludos, ${sesion}`;
+
+    // div.textContent = sessionStorage.getItem("Sesión");
+})
