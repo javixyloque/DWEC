e73ebb8ad76710f98fab9dc6fc9ac238e2bd7aa4
javixyloque <perosa1000@gmail.com>
2025-01-24 13:58:39 +0100

    react posts

--- 08-react/posts/src/Posts.jsx
+++ 08-react/posts/src/Posts.jsx
@@ -1,42 +1,47 @@
 import { useState, useEffect } from 'react';
 
-function Posts () {
+function Posts() {
+    // STATE PARA GUARDAR LOS POSTS Y LOS USUARIOS 
+    // SETSTATE PARA ACTUALIZAR LOS VALORES DE LOS STATES (POSTS Y USERS)
     const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [users, setUsers] = useState([]);
 
-    useEffect( () => {
-        //NO PUEDES PASAR UNA FUNCION ASINCRONA A USEEFFECT, POR ESO SE CREA LA ASÍNCRONA DENTRO
-        async function fetchPosts() {
-            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-            // NO LE PONEMOS POSTS PARA NO ENMASCARAR LA VARIABLE DE ARRIBA POR SI LA NECESITAMOS
-            const postList = await res.json();
+    useEffect(() => {
+        async function fetchAll() {
+            const resPost = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const postList = await resPost.json();
+
+            const resUser = await fetch('https://jsonplaceholder.typicode.com/users');
+            const userList = await resUser.json();
+
+            setUsers(userList);
             setPosts(postList);
-            setLoading(false);
         }
-        fetchPosts();
-    }, []);
+        fetchAll();
+        // DEPENDENCIA POSTS => CADA VEZ QUE CAMBIA SE LLAMA A ESTA FUNCIÓN 
+    }, [posts]); 
 
-    return  (
+    return (
+        //NO HACE FALTA PERO PARA TENER UN ELEMENTO RAIZ
         <>
-            <div className='post-list'>
-                {/* PARA INYECTAR CODIGO JS EN JSX NECESITAMOS LLAVES  */}
-                {
-                    // PARA OBTENER UN ARRAY NECESITAMOS UN .MAP
-                    posts.map(post => {
-                        return (
-                            <div className='post' key={post.id}>
-                                <h1>{post.title}</h1>
-                                <p>{post.body}</p>
-                            </div>
-                        );
-                    })
-                }
+            <div className="post-list">
+                {posts.map(post => {
+                    // FIND => DEVUELVE EL USUARIO CON EL ID = POST.USERID
+                    const author = users.find(user => /*return implicito*/ user.id === post.userId ); 
+
+                    return (
+                        <div className="post" key={post.id}>
+                            <h1>{post.title}</h1>
+                            <p>{post.body}</p>
+                            {/* SI EXISTE AUTOR => NOMBRE, SI NO => SIN AUTOR */}
+                            <p>Author: {author ? author.name : 'Sin autor'}</p> 
+                        </div>
+                    );
+                })}
             </div>
         </>
-    )
-
-    
+    );
 }
 
 
-export {Posts}
+export { Posts };
