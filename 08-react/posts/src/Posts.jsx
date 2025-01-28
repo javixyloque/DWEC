52c6b4128f3dc2371fe4f7a01c4fce5ef98350cd
javixyloque <perosa1000@gmail.com>
2025-01-28 14:09:12 +0100

    Commit final

--- 08-react/posts/src/Posts.jsx
+++ 08-react/posts/src/Posts.jsx
@@ -5,27 +5,51 @@ function Posts() {
     // SETSTATE PARA ACTUALIZAR LOS VALORES DE LOS STATES (POSTS Y USERS)
     const [posts, setPosts] = useState([]);
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
 
     useEffect(() => {
         async function fetchAll() {
             const resPost = await fetch('https://jsonplaceholder.typicode.com/posts');
             const postList = await resPost.json();
 
-            const resUser = await fetch('https://jsonplaceholder.typicode.com/users');
-            const userList = await resUser.json();
-
-            setUsers(userList);
+            
             setPosts(postList);
+            setLoading(false);
         }
         fetchAll();
         // DEPENDENCIA POSTS => CADA VEZ QUE CAMBIA SE LLAMA A ESTA FUNCIÓN 
     }, [posts]); 
 
+    useEffect(() => {
+        // SI CAMBIA LA CANTIDAD DE POSTS, LIMPIAR EL STATE DE ALBUMS
+        async function fetchUserNames() {
+            
+            let uniqueUserIds = [...new Set(posts.map ( post => post.userId ))];
+            // LOS 3 PUNTOS EXPANDEN EL ITERABLE, LO COPIAN POR ASÍ DECIRLO¡
+            // PETICIONES ASINCRONAS A LOS USUARIOS
+            let resUser =  uniqueUserIds.map (async idUser => {
+
+                const respuesta = await fetch(`https://jsonplaceholder.typicode.com/users/${idUser}`);
+                const devolver = await respuesta.json();
+                return devolver;
+
+            });
+            const autores = await Promise.all(resUser);
+            // const nombresAutores = autores.map(autor =>autor.name)
+            setUsers(autores);
+            setLoading(false);
+        }
+        fetchUserNames();
+    }, []);
+
     return (
         //NO HACE FALTA PERO PARA TENER UN ELEMENTO RAIZ
         <>
             <div className="post-list">
-                {posts.map(post => {
+                {
+                
+                posts.map(post => {
                     // FIND => DEVUELVE EL USUARIO CON EL ID = POST.USERID
                     const author = users.find(user => /*return implicito*/ user.id === post.userId ); 
 
